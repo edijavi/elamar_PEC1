f7c15835bd801157b8e74e4374dcf8cef342a4d0
edijavi <edilata@msn.com>
2022-10-16 23:09:09 +0200

    - script.js: local storage data show in UI

--- PEC1_Ej2_3/script.js
+++ PEC1_Ej2_3/script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = movieSelect.value;
 
 //Save selected movie index and price
@@ -29,7 +31,23 @@ function updateSelectedCount() {
 }
 
 
+//Get data from localStorage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
 
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+        
+    }
+}
 
 
 //Movie select event
@@ -49,3 +67,5 @@ container.addEventListener('click', e =>{
         updateSelectedCount();
     }
 });
+
+updateSelectedCount();

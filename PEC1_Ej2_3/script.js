5f522c973259e8c2384ddf212b18863bfc1219fd
edijavi <edilata@msn.com>
2022-10-16 22:34:03 +0200

    - script.js: Select seats and counts implementation, event listeners

---
+++ PEC1_Ej2_3/script.js
@@ -0,0 +1,37 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSlect = document.getElementById('movie');
+
+let ticketPrice = movieSlect.value;
+
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    
+    const selectedSeatsCount = selectedSeats.length;
+    
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+
+
+
+
+//Movie select event
+movieSlect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+});
+
+//Seat click event
+container.addEventListener('click', e =>{
+    if (
+        e.target.classList.contains('seat') && 
+        !e.target.classList.contains('occupied')
+    ) {
+        e.target.classList.toggle('selected');
+        updateSelectedCount();
+    }
+});

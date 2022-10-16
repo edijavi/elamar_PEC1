d3b121a1b79a120d4b56cbd9d6e04a332d8e2d4d
edijavi <edilata@msn.com>
2022-10-16 22:55:41 +0200

    -script.js: saving in the local storage

--- PEC1_Ej2_3/script.js
+++ PEC1_Ej2_3/script.js
@@ -2,17 +2,30 @@ const container = document.querySelector(".container");
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
-const movieSlect = document.getElementById('movie');
+const movieSelect = document.getElementById('movie');
 
-let ticketPrice = movieSlect.value;
+let ticketPrice = movieSelect.value;
+
+//Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+
+}
 
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
     
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
+
+    setMovieData(movieSelect.selectedIndex, movieSelect.value)
 }
 
 
@@ -20,8 +33,9 @@ function updateSelectedCount() {
 
 
 //Movie select event
-movieSlect.addEventListener('change', e => {
+movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
 

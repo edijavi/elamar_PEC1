4473d8dad5070683a9d0ae2244c2ed102924abf4
edijavi <edilata@msn.com>
2022-10-16 08:07:35 +0200

    - Convertion calculation implementation calculate() - Swap values - Allow only posite numbers

--- PEC1_Ej2_2/script.js
+++ PEC1_Ej2_2/script.js
@@ -1,6 +1,36 @@
-function calculate(params) {
-    fetch('items.json').then(res => res.json())
-    .then(data => (document.body.innerHTML = data[0].text));
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEL = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+function calculate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value
+    fetch(`https://v6.exchangerate-api.com/v6/570e43e0f5bbeb36ff4f12a3/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data =>{
+        //console.log(data);
+        const rate = data.conversion_rates[currency_two] / data.conversion_rates[currency_one];
+        rateEL.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+        amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
+    })
+
 }
+
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('imput', calculate);
+
+swap.addEventListener('click', () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    calculate();
+});
+
 
 calculate()
